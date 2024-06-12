99b973b3417d88de4cc840ff56d0e584c49a801a
Niix <46878004+Niix-Dan@users.noreply.github.com>
2024-06-12 11:49:23 -0300

    Create array.js

---
+++ src/array.js
@@ -0,0 +1,43 @@
+/**
+ * Mean of an Array
+ * @param {number[]} array The array of numbers
+ * @returns {number} The mean of the array
+ */
+modules.exports.mean = (array) => {
+    return array.reduce((a, b) => a + b, 0) / array.length;
+}
+
+/**
+ * Median of an Array
+ * @param {number[]} array The array of numbers
+ * @returns {number} The median of the array
+ */
+module.exports.median = (array) => {
+    const sorted = array.slice().sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+    return sorted.length % 2 === 0 ? (sorted[mid - 1] + sorted[mid]) / 2 : sorted[mid];
+}
+
+/**
+ * Mode of an Array
+ * @param {number[]} array The array of numbers
+ * @returns {number[]} The mode of the array
+ */
+module.exports.mode = (array) => {
+    const frequency = {};
+    array.forEach(value => frequency[value] = (frequency[value] || 0) + 1);
+    const maxFreq = Math.max(...Object.values(frequency));
+    return array.filter(value => frequency[value] === maxFreq);
+}
+
+/**
+ * Standard Deviation of an Array
+ * @param {number[]} array The array of numbers
+ * @returns {number} The standard deviation of the array
+ */
+module.exports.standardDeviation = (array) => {
+    const avg = mean(array);
+    const squareDiffs = array.map(value => Math.pow(value - avg, 2));
+    const avgSquareDiff = mean(squareDiffs);
+    return Math.sqrt(avgSquareDiff);
+}
